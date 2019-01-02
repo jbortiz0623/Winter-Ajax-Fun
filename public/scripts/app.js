c921689cb6be99fb575c03b7a16df3c30d6485d3
jbortiz0623 <jbortiz0623@gmail.com>
2019-01-01 17:41:29 -0800

    Initialized Ajax call GET

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,28 @@
 $(document).ready(() => {
     console.log('Hello Js & JQ!')
+
+    $.ajax({
+        method: 'GET',
+        url: 'http://localhost:3000/api/foods',
+        success: onSuccess,
+        error: onError
+    });
+    function onSuccess(foods) {
+        innerHTML='';
+        foods.forEach((onefood)=> {
+            console.log('testing food', onefood)
+            console.log('testing ingredients', onefood.ingredients[0]);
+            innerHTML += `<div>`;
+            innerHTML += `<h1>`+onefood.name+`</h1>`;
+            innerHTML += `<p>`+onefood.ingredients[0].title+`</p>`;
+            innerHTML += `<p>`+onefood.ingredients[0].origin+`</p>`;
+
+            innerHTML+='</div>'
+            
+        })
+        $('#foodTarget').append(innerHTML);
+    }
+    function onError(error){
+        console.log(error);
+      }
 });
